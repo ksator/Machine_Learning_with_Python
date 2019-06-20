ef0589cee7a664c95cdea6151fb1014da7d396f8
Khelil Sator <khelil.sator.git@gmail.com>
2019-06-20 14:00:42 +0200

    Create recursive_feature_elimination.py

---
+++ recursive_feature_elimination.py
@@ -0,0 +1,34 @@
+# Load LinearSVC class from Scikit Learn library 
+# LinearSVC is similar to SVC with parameter kernel='linear'
+# LinearSVC performs classification
+# LinearSVC finds the linear separator that maximizes the distance between itself and the closest/nearest data point point
+from sklearn.svm import LinearSVC
+
+# load RFE (Recursive Feature Elimination) 
+# RFE is used to remove features
+from sklearn.feature_selection import RFE
+
+# load the iris dataset
+from sklearn import datasets
+dataset = datasets.load_iris()
+
+# the dataset has 150 items, each item has 4 features (sepal length, sepal width, petal length, petal width) 
+dataset.data.shape
+dataset.feature_names
+
+# instanciate the LinearSVC class   
+svm = LinearSVC(max_iter=5000)
+
+# instanciate the RFE class 
+# select the number of features to keep (3 in that example) 
+# select the classifier model to use  
+rfe = RFE(svm, 3)
+
+# use the iris dataset and fit 
+rfe = rfe.fit(dataset.data, dataset.target)
+
+# print summaries for the selection of attributes
+# sepal length is not selected. The 3 selected features are sepal width, petal length, petal width 
+print(rfe.support_)
+print(rfe.ranking_)
+
